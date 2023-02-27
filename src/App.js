e051fefc889a6f40e4490c93ed77a38f996747f0
HieuPham2000 <hieu.pt183535@gmail.com>
2023-02-27 23:20:04 +0700

    feat: cấu hình router, cơ chế tải layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        // const Layout = route.layout === null ? Fragment : DefaultLayout; // undefined == null => dùng 3 dấu =
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
