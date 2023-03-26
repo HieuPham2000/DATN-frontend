7988b0c24c217f00fa345918b8ffcb287dfb2f76
HieuPham2000 <hieu.pt183535@gmail.com>
2023-03-26 21:33:44 +0700

    feat: thêm page 404, 500

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import ForgotPassword from '~/pages/ForgotPassword';
 import Home from '~/pages/Home';
 import Login from '~/pages/Login';
+import Page404 from '~/pages/Page404';
+import Page500 from '~/pages/Page500';
 import Register from '~/pages/Register';
 
 // Public routes
@@ -9,6 +11,8 @@ const publicRoutes = [
     { path: '/login', component: Login, layout: null },
     { path: '/register', component: Register, layout: null },
     { path: '/forgot-password', component: ForgotPassword, layout: null },
+    { path: '/500', component: Page500, layout: null },
+    { path: '*', component: Page404, layout: null },
 ];
 
 const privateRoutes = [];
