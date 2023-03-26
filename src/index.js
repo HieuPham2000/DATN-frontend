f9340b8d2d1b109283e31fae84aa14794f324ef9
HieuPham2000 <hieu.pt183535@gmail.com>
2023-03-26 16:22:41 +0700

    feat: thêm custom theme, service worker cho pwa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '~/App';
+import * as serviceWorkerRegistration from '~/serviceWorkerRegistration';
 import reportWebVitals from '~/reportWebVitals';
 import GlobalStyles from '~/components/GlobalStyles';
+import GlobalTheme from '~/components/GlobalTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <GlobalStyles>
-            <App />
+            <GlobalTheme>
+                <App />
+            </GlobalTheme>
         </GlobalStyles>
     </React.StrictMode>,
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/scripts/base/theme.js
@@ -0,0 +1,88 @@
+const getDesignTheme = (mode) => ({
+    palette: {
+        mode,
+        ...(mode === 'dark' ? {
+          background: {
+            default: '#161C24',
+            paper: '#161C24',
+          }
+        } : {}),
+        primary: {
+            light: '#5BE584',
+            main: '#00AB55',
+            dark: '#007B55',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#84A9FF',
+            main: '#3366FF',
+            dark: '#1939B7',
+            contrastText: '#fff',
+        },
+        info: {
+            light: '#61F3F3',
+            main: '#00B8D9',
+            dark: '#006C9C',
+            contrastText: '#fff',
+        },
+        success: {
+            light: '#86E8AB',
+            main: '#36B37E',
+            dark: '#1B806A',
+            contrastText: '#fff',
+        },
+        warning: {
+            light: '#86E8AB',
+            main: '#36B37E',
+            dark: '#1B806A',
+            contrastText: '#fff',
+        },
+        error: {
+            light: '#FFAC82',
+            main: '#FF5630',
+            dark: '#B71D18',
+            contrastText: '#fff',
+        },
+    },
+    typography: {
+        htmlFontSize: 10,
+        h1: {
+            fontWeight: 700,
+            fontSize: '6.4rem',
+            lineHeight: 1.25,
+            letterSpacing: '0em',
+        },
+        h2: {
+            fontWeight: 700,
+            fontSize: '4.8rem',
+            lineHeight: 1.334,
+            letterSpacing: '0em',
+        },
+        h3: {
+            fontWeight: 700,
+            fontSize: '3.2rem',
+            lineHeight: 1.5,
+            letterSpacing: '0em',
+        },
+        h4: {
+            fontWeight: 700,
+            fontSize: '2.4rem',
+            lineHeight: 1.5,
+            letterSpacing: '0em',
+        },
+        h5: {
+            fontWeight: 700,
+            fontSize: '2rem',
+            lineHeight: 1.5,
+            letterSpacing: '0em',
+        },
+        h6: {
+            fontWeight: 700,
+            fontSize: '1.8rem',
+            lineHeight: 1.556,
+            letterSpacing: '0em',
+        },
+    },
+});
+
+export { getDesignTheme };
