3692fbbd28d0081a1b39f00363fe5a61d017fe6d
HieuPham2000 <hieu.pt183535@gmail.com>
2023-05-01 16:04:35 +0700

    feat: test chuyển logic check error vào PasswordTextField

--- src/components/PasswordTextField/PasswordTextField.js
+++ src/components/PasswordTextField/PasswordTextField.js
@@ -1,16 +1,26 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { IconButton, InputAdornment, TextField } from '@mui/material';
 import { Visibility as VisibilityIcon, VisibilityOff as VisibilityOffIcon } from '@mui/icons-material';
+import { validateWithRules } from '~/utils/common/validate/validate';
 
-
-function PasswordTextField({ id, label, value, onChange, error, title, helperText, ...rest}) {
+function PasswordTextField({ id, label, value, onChange, validateRules, ...rest }) {
     const [show, setShow] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const needValidate = useRef(false);
 
     const handleClickShow = () => setShow((show) => !show);
 
     const handleMouseDown = (e) => {
         e.preventDefault();
     };
+
+    useEffect(() => {
+        if (needValidate.current && validateRules) {
+            let [, msg] = validateWithRules(validateRules, value, { label });
+            setErrorMsg(msg);
+        }
+        needValidate.current = true;
+    }, [value, label, validateRules]);
 
     return (
         <Fragment>
@@ -35,9 +45,9 @@ function PasswordTextField({ id, label, value, onChange, error, title, helperTex
                     ),
                 }}
                 value={value}
-                error={error}
-                title={title}
-                helperText={helperText}
+                error={!!errorMsg}
+                title={errorMsg}
+                helperText={errorMsg}
                 onChange={onChange}
             />
         </Fragment>
