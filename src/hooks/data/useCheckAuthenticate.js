552d3259e04abdf343bb0f66c04af019a1e6f25c
HieuPham2000 <hieu.pt183535@gmail.com>
2023-06-29 09:30:40 +0700

    feat: bỏ import thừa

--- src/hooks/data/useCheckAuthenticate.js
+++ src/hooks/data/useCheckAuthenticate.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { HttpStatusCode } from 'axios';
+// import { HttpStatusCode } from 'axios';
 import { checkAuthenticate } from '~/services/accountService';
 
 const useCheckAuthenticate = () =>
@@ -14,7 +14,7 @@ const useCheckAuthenticate = () =>
         // retry: (failureCount, error) => {
         //     return error?.response?.status !== HttpStatusCode.Unauthorized && failureCount <= 3 ? true : false;
         // },
-        retry: false
+        retry: false,
     });
 
 export default useCheckAuthenticate;
